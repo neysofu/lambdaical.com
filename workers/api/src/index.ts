76d10e3ec15fd40629b4949cacd2186680f6598b
Filippo Costa <filippo@neysofu.me>
2023-01-01 01:59:54 +0100

    Fix some stuff

--- workers/api/src/index.ts
+++ workers/api/src/index.ts
@@ -1,63 +1,15 @@
 import * as ical from "ical.js";
 import { Base64 } from 'js-base64';
-import { Validator } from 'jsonschema';
 
 export interface Env { }
-
-// Operators
-// Filter-event-by-attribute-contains
-// Filter-event-by-attribute-regex
-// Filter-event-by-date
-// Merge
-
-interface Merge {
-	kind: 'merge';
-	calendars: Calendar[];
-}
-
-type LogicQuantifier = 'all' | 'any';
-
-interface ComponentFilter {
-	kind: 'component-filter';
-	nameFilter: StringFilter;
-	quantifier: LogicQuantifier;
-	propertyFilter: PropertyFilter;
-}
-
-interface StringFilter {
-	kind: 'string-filter';
-	// regex
-}
-
-interface PropertyFilter {
-	kind: 'property-filter';
-	nameFilter: StringFilter;
-	valueFilter: StringFilter;
-}
-
-interface Calendar {
-	url: string;
-}
 
 // Function that takes a Url as input and parses the rules, e.g.
 // https://example.com/calendar.ics/base64-of-src-url/merge/another-base64/filter-attribute-contains-foo/filter-attribute-regex-foo
 // =foo&keep=bar&keep=baz
 
-async function apiV0(base64payload: string): Promise<Response> {
-	const payload = Base64.decode(base64payload);
-	const { searchParams } = new URL(request.url)
-	const searchParamsArray = Array.from(searchParams.entries());
-	let keep = searchParams.get('keep')
-	if (!keep) {
-		return new Response("No keep parameter provided", { status: 400 })
-	}
-	let srcCalendarUrl = searchParams.get('src')
-	if (srcCalendarUrl) {
-		let icalResponse = await fetch(srcCalendarUrl);
-		var icalRaw = await icalResponse.text();
-	} else {
-		var icalRaw = await request.text();
-	}
+async function apiV0(srcCalendarUrl: string, keep: string): Promise<Response> {
+	let icalResponse = await fetch(srcCalendarUrl);
+	var icalRaw = await icalResponse.text();
 	var iCalContents = ical.parse(icalRaw);
 	let subcomponents = iCalContents[2];
 	let filteredSubcomponents = subcomponents.filter((data) => {
@@ -77,6 +29,14 @@ export default {
 		ctx: ExecutionContext
 	): Promise<Response> {
 		const url = new URL(request.url)
-		url.
+		const src = url.searchParams.get('s');
+		const keep = url.searchParams.get('keep');
+		if (!src) {
+			return new Response("No src parameter provided", { status: 400 })
+		}
+		if (!keep) {
+			return new Response("No keep parameter provided", { status: 400 })
+		}
+		return await apiV0(src, keep);
 	},
 };
